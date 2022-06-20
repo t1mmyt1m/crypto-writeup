79bc088697e30f40d5cc21c6b9fee160c8065e8c
t1mmy_t1m <leechaechae2006@gmail.com>
2022-06-20 09:03:14 +0900

    Update reverse.py

--- python crypto/reverse.py
+++ python crypto/reverse.py
@@ -1,9 +1,10 @@
-msg='My name is t1mmyt1m and i am stupid student.'
+msg=input('Enter message: ')
 translated='' 
 
 i=len(msg)-1
 while i>=0:
     translated+=msg[i]
+    #print('i is', i, ',msg[i] is', msg[i], ',translated is', translated)
     i-=1
 
 print(translated)
